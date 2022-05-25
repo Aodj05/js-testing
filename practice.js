a87b31be67c257b7705600cb03bb8cf53cf3eb7f
Aodj05 <aodj05@msn.com>
2022-05-25 17:46:55 -0400

    cipher test

--- practice.js
+++ practice.js
@@ -8,6 +8,38 @@ function reverseString(string) {
   return string.split("").reverse().join('');
 }
 
+const calc = {
+	add: (a, b) => a + b,
+	sub: (a, b) => a - b,
+	div: (a, b) => a / b,
+	mult: (a, b) => a * b
+}
+
+function cipher(string, shift) {
+	const resultArray = [];
+	for (let i = 0; i < string.length; i++) {
+		const code = string.charCodeAt(i) + shift || 
+		string.charCodeAt(i).toUpperCase;
+		while (code > 122) {
+			code = (code - 122) + 96;
+		}
+		resultArray.push(String.fromCharCode(code));
+	}
+	return resultArray.join('');
+}
+
+const object = array([1,8,3,4,2,6]);
+
+object == {
+	average: 4,
+	min: 1,
+	max: 8,
+	length: 6
+};
+
 module.exports = { cap,
-	reverseString
+	reverseString,
+	calc,
+	cipher,
+	array
 };
--- practice.test.js
+++ practice.test.js
@@ -1,6 +1,9 @@
 const practice = require('./practice');
 const cap = practice.cap;
 const reverseString = practice.reverseString;
+const calc = practice.calc;
+const cipher =  practice.cipher;
+const array = practice.object;
 
 test('first letter captitalized', () => {
   expect(cap("justin")).toBe("Justin");
@@ -8,4 +11,36 @@ test('first letter captitalized', () => {
 
 test('reverse a string', () => {
   expect(reverseString("nitsuJ")).toBe("Justin");
+});
+
+test('add 1 + 2 to equal 3', () => {
+  expect(calc.add(1, 2)).toEqual(3);
+});
+
+test('subtract 2 - 1 to equal 1', () => {
+  expect(calc.sub(2, 1)).toEqual(1);
+});
+
+test('divide 6 by 2 to equal 3', () => {
+  expect(calc.div(6, 2)).toEqual(3)
+});
+
+test('multiply 3 * 2 to equal 6', () => {
+	expect(calc.mult(3, 2)).toEqual(6);
+});
+
+test('cipher shifts', () => {
+	expect(cipher("these are shifted", 7)).toBe("aolzl'hyl'zopmalk");
+});
+
+test('cipher shifts with capitals', () => {
+	expect(cipher("These Are Shifted", 7)).toBe("[olzl'Hyl'Zopmalk");
+});
+
+test('cipher shifts with symbols', () => {
+	expect(cipher("These Are Shifted !@#$%", 7)).toBe("[olzl'Hyl'Zopmalk'(G*+,");
+});
+
+test('Array is averaged', () => {
+  expect(array.object['average']).toBe(4);
 });
